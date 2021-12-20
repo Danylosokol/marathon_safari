42f5337b4257fa5d57746891f5be2dc7b07b7e2a
Danylo Sokol <dansokol677@gmail.com>
2021-12-20 14:17:13 +0100

    finished galary and started tour section

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ $(function(){
 
     fixedContentPos: false,
   });
+  $(".gallery__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"> <img src="images/arrow-left.svg" alt=""> </button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"> <img src="images/arrow-right.svg" alt=""> </button>',
+  });
 });
